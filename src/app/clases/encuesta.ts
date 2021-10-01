0de1154a804a8c3e8fcd6ed36b82da234a330940
dfedericovera@gmail.com <dfedericovera@gmail.com>
2021-10-01 16:47:05 -0300

    Agrego servicio encuesta

---
+++ src/app/clases/encuesta.ts
@@ -0,0 +1,18 @@
+
+export class Encuesta
+{
+  public id: string;
+  public satisfaccion:number;
+  public ocio: string;
+  public tipoJuegoPreferido: string;
+  public sujerencia: string;
+
+  constructor()
+  {
+      
+  }
+
+
+
+
+}
---
+++ src/app/services/encuesta.service.ts
@@ -0,0 +1,86 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Encuesta } from '../clases/encuesta';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EncuestaService
+{
+
+  public encuestas: Array<Encuesta>;
+  constructor(private db: AngularFirestore)
+  {
+  }
+
+  ngOnInit()
+  {
+  }
+
+  //Devuelve un Observable de tipo Encuesta Array.
+  getEncuestas(): Observable<Encuesta[]>
+  {
+    console.log("Get encuestas;")
+    return this.db.collection("encuestas", (ref) =>
+      ref.orderBy('date')).snapshotChanges().pipe(
+        map((snaps) =>
+          snaps.map((snap) =>
+          {
+            return snap.payload.doc.data() as Encuesta;
+          }))
+      )
+  }
+
+  //Metodo para crear un nuevo Encuesta en la DB
+  addEncuesta(encuesta: Encuesta)
+  {
+    //?Con esto FireBase se encarga de todo,
+    //?no hay que pensar en endpoints o si esta o no creada la tabla.
+    //?Adicionamos un nuevo record a la tabla.
+    console.log(encuesta);
+    try
+    {
+      return new Promise<any>((resolve, reject) =>
+      {
+        this.db
+          .collection("encuestas")
+          .add(JSON.parse(JSON.stringify(encuesta)))
+          .then(res => { console.log('Guardado---', res) }, err => reject(console.error(err)));
+      });
+    } catch (error)
+    {
+      console.error('Error: ', error);
+    }
+
+  }
+
+  //Borrar un Encuesta de la DB
+  deleteEncuesta(encuesta: Encuesta)
+  {
+    try
+    {
+      return this.db
+        .collection("encuestas")
+        .doc(encuesta.id)
+        .delete()
+        .then(res => { console.log(res) });
+
+    } catch (error)
+    {
+      console.log('Error: ', error);
+    }
+
+  }
+
+  //Editar un Encuesta
+  editEncuesta(newEncuesta)
+  {
+    return this.db
+      .collection("encuestas")
+      .doc(newEncuesta.payload.doc.id)
+      .set({ completed: true }, { merge: true });
+
+  }
+}
